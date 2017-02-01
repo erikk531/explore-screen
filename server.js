75dab4775d2df25fb46fef457edc5880550fef34
Erik Koebke <erikkoebke@Eriks-MacBook-Pro.local>
2017-02-01 16:42:50 -0500

    updated server

--- server.js
+++ server.js
@@ -3,19 +3,26 @@ var bodyParser = require('body-parser');
 var dataCollector = require('./dataCollector');
 var app = express();
 app.use(bodyParser.json());
-var allowCrossDomain = function(req, res, next) {
-    if ('OPTIONS' == req.method) {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-      res.sendStatus(200);
-    }
-    else {
-      next();
-    }
-};
 
-app.use(allowCrossDomain);
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.post('/merchants', function(request, response) {
     var body = request.body;
