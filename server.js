a853a1a2c587332e2392ab2e480cb97e17bb5162
Erik Koebke <erikkoebke@Eriks-MacBook-Pro.local>
2017-02-01 16:38:00 -0500

    updated

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ var allowCrossDomain = function(req, res, next) {
     if ('OPTIONS' == req.method) {
       res.header('Access-Control-Allow-Origin', '*');
       res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-      res.send(200);
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+      res.sendStatus(200);
     }
     else {
       next();
